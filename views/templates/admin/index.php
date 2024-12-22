943522afa89276e3c60be5c56a60874b87fa3956
Nisaba <34550856+Nisaba@users.noreply.github.com>
2024-12-22 15:28:33 +0000

    Prestashop Validator Update 2

--- views/templates/admin/index.php
+++ views/templates/admin/index.php
@@ -1,5 +1,21 @@
 <?php
-
+/**
+ * Copyright (c) 2024 Swiss Bitcoin Pay (https://swiss-bitcoin-pay.ch)
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License version 3.0
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * @author    Swiss Bitcoin Pay <https://swiss-bitcoin-pay.ch>
+ * @copyright 2024 Swiss Bitcoin Pay
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License version 3.0
+*/
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
 
